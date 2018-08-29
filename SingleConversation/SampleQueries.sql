44a70131938d5def0d581ce000ae50677bb744e5
ddschmitz <ddschmitz@pluto.dsu.edu>
2018-08-29 17:49:48 -0500

    Removed real names.  Oops.

--- SingleConversation/SampleQueries.sql
+++ SingleConversation/SampleQueries.sql
@@ -8,7 +8,7 @@ GROUP BY M.SenderID
 SELECT P.Name, M.Timestamp, M.Content
 FROM Message AS M
 INNER JOIN Participant AS P ON M.SenderID = P.ID
-WHERE P.Name = "Chad Horack"
+WHERE P.Name = "John Doe"
 ORDER BY M.Timestamp DESC
 LIMIT 1
 
@@ -16,7 +16,7 @@ LIMIT 1
 Select DISTINCT DATE(M.Timestamp), COUNT(*)
 FROM Message AS M
 INNER JOIN Participant AS P ON M.SenderID = P.ID
-WHERE P.Name = "Mike Janish"
+WHERE P.Name = "John Doe"
 GROUP BY DATE(M.Timestamp)
 
 # Number of reactions to each participants messages
